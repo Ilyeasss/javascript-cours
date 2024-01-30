95e39644d547e30eb10ce5056514d4b105275d37
Ilyeasss <ilyasbouelbaraka@gmail.com>
2024-01-30 10:15:52 +0100

    first test

---
+++ Basics/index.js
@@ -0,0 +1,12 @@
+console.log("hello java")
+alert("ALERT!!")
+function greet(){
+    console.log("salut")
+}
+function somme(n,b){
+    return n+b
+}
+console.log(somme(12,5))
+console.log(greet())
+let x = 5
+x =+ 2
---
+++ conditions/index.js
@@ -0,0 +1,26 @@
+let a = 3;
+let b = "3";
+let x = "done";
+let y = "not done"
+
+
+if (a==b) {
+    console.log("okay");
+}
+else{
+    console.log("not okay");
+};
+
+if (a!=b){
+    console.log("YES!")
+}
+else{
+    console.log("NO!")
+};
+
+if (a==b && y==x){
+    console.log("first example")
+}
+else if (a==b || y==x) {
+    console.log("second example")
+};
